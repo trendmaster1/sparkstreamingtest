7d2914a97dcf049d850c6ff28f5d0b1d70548561
Bruce Wang <bing.wang@xiaohulu.com>
2017-04-20 17:22:25 +0800

    Update.

--- sparkstreamingtest.scala
+++ sparkstreamingtest.scala
@@ -4,9 +4,9 @@ import org.apache.spark.streaming.StreamingContext._
 
 object sparkstreamingtest {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setMaster("local").setAppName("sparkstreamingtest")
+    val conf = new SparkConf().setMaster("local[2]").setAppName("sparkstreamingtest")
     val ssc = new StreamingContext(conf, Seconds(1))  
-    val lines = ssc.socketTextStream("master", 9999)  
+    val lines = ssc.socketTextStream("127.0.0.1", 9999)  
       
     val words = lines.flatMap(_.split(" "))  
     val pairs = words.map(word => (word, 1))  
